e76597db84cec367030adc176695a9754e44b185
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2021-10-29 16:14:24 +0300

    board added

--- angular-cordova/src/app/app.module.ts
+++ angular-cordova/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CellComponent } from './cell/cell.component';
 import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CellComponent,
     BoardComponent
   ],
   imports: [
--- angular-cordova/src/app/board/board.component.ts
+++ angular-cordova/src/app/board/board.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+  cells: string[][] = [];
 
   constructor() { }
 
+  checkerClick(row_index: number, column_index: number): void {
+    this.cells[row_index][column_index] = "Black";
+  }
+
   ngOnInit(): void {
+    this.board_init();
+    this.checkers_init();
+  }
+
+  board_init(): void {
+    let nextWhite: Boolean = true;
+    for(let row: number = 0; row < 8; row++) {
+      this.cells[row] = [];
+      let currWhite: Boolean = nextWhite;
+      nextWhite = !nextWhite;
+      for(let column: number = 0; column < 8; column++) {
+        this.cells[row][column] = currWhite ? "White" : "Black";
+        currWhite = !currWhite;
+      }
+    }
+    console.log(this.cells)
+  }
+
+  checkers_init(): void {
+    for(let column: number = 1; column < 8; column += 2) {
+      this.cells[0][column] = "Black_Black";
+    }
+
+    this.cells[7][4] = "Black_White";
   }
 
 }
