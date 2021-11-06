38375e34f23c53131ffe76a7df45a5be67870c11
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2021-11-06 13:20:32 +0200

    ai performance improved

--- angular-cordova/src/app/board/services/AI/ai.service.ts
+++ angular-cordova/src/app/board/services/AI/ai.service.ts
@@ -24,23 +24,31 @@ export class AiService {
     return [bestDecision[0], bestDecision[1]];
   }
 
+  // returns tuple of format: [move_from, move_to, pointsOfBestDecision]
   private minimax(depth: number, isMax: Boolean, cells: string[][], rabbit_cell: [number, number], wolf_cells: [number,number][]): [[number,number], [number,number], number] {
     if(depth === 0) {
       return [[-1,-1],[-1,-1],this.brainService.calculate_points(cells, rabbit_cell)];
     }
 
+    let best_from: [number,number] = [-1,-1];
+    let best_to: [number,number] = [-1,-1];
+
     if(isMax) {
       let maxEval: number = -Infinity;
-      let best_from: [number,number] = [-1,-1];
-      let best_to: [number,number] = [-1,-1];
+
       const rabbitMoves = this.verificationService.get_rabbit_moves(cells, rabbit_cell[0], rabbit_cell[1]);
       for(const move of rabbitMoves) {
         if(move) {
-          const cpy_cells: string[][] = cells.map((row) => row.map((cell) => cell));
-          cpy_cells[move[0]][move[1]] = "Black_White";
-          cpy_cells[rabbit_cell[0]][rabbit_cell[1]] = "Black";
+          const cells_backup: [number,number,string][] = [[rabbit_cell[0], rabbit_cell[1], "Black_White"],[move[0],move[1],"Black"]];
 
-          const points: number = this.minimax(depth - 1, false, cpy_cells, move, wolf_cells)[2];
+          cells[move[0]][move[1]] = "Black_White";
+          cells[rabbit_cell[0]][rabbit_cell[1]] = "Black";
+
+          const points: number = this.minimax(depth - 1, false, cells, move, wolf_cells)[2];
+
+          cells[cells_backup[0][0]][cells_backup[0][1]] = cells_backup[0][2];
+          cells[cells_backup[1][0]][cells_backup[1][1]] = cells_backup[1][2];
+
           if(points > maxEval) {
             maxEval = points;
             best_from = rabbit_cell;
@@ -52,19 +60,24 @@ export class AiService {
     }
     else {
       let minEval: number = Infinity;
-      let best_from: [number,number] = [-1,-1];
-      let best_to: [number,number] = [-1,-1];
 
       for(let i=0;i<4;i++) {
         const wolfesMoves = this.verificationService.get_wolf_moves(cells, wolf_cells[i][0], wolf_cells[i][1]);
         for(const move of wolfesMoves) {
           if(move) {
-            const cpy_wolfes_cells: [number,number][] = [[...wolf_cells[0]],[...wolf_cells[1]],[...wolf_cells[2]],[...wolf_cells[3]]];
-            cpy_wolfes_cells[i] = move;
-            const cpy_cells: string[][] = cells.map((row) => row.map((cell) => cell));
-            cpy_cells[move[0]][move[1]] = "Black_Black";
-            cpy_cells[wolf_cells[i][0]][wolf_cells[i][1]] = "Black";
-            const points: number = this.minimax(depth - 1, true, cpy_cells, rabbit_cell, cpy_wolfes_cells)[2];
+            const backup_wolf_cell: [number,number] = wolf_cells[i];
+            wolf_cells[i] = move;
+
+            const cells_backup: [number,number,string][] = [[wolf_cells[i][0], wolf_cells[i][1], "Black_Black"],[move[0],move[1],"Black"]];
+            cells[move[0]][move[1]] = "Black_Black";
+            cells[wolf_cells[i][0]][wolf_cells[i][1]] = "Black";
+
+            const points: number = this.minimax(depth - 1, true, cells, rabbit_cell, wolf_cells)[2];
+
+            wolf_cells[i] = backup_wolf_cell;
+            cells[cells_backup[0][0]][cells_backup[0][1]] = cells_backup[0][2];
+            cells[cells_backup[1][0]][cells_backup[1][1]] = cells_backup[1][2];
+
             if(points < minEval) {
               minEval = points;
               best_from = wolf_cells[i];
